1b4df2269359f1733e8cc3acaa3aa68d64c8b48f
David2093 <dnesterevych@gmail.com>
2025-10-05 00:39:21 +0300

    fghdgfhfghfdgyturnmbvnnbvmbnvvbnm

--- modal.js
+++ modal.js
@@ -1,14 +1,18 @@
-    const openModal = document.getElementById("openModal");
-    const closeModal = document.getElementById("closeModal");
-    const modalOverlay = document.getElementById("modalOverlay");
-    openModal.addEventListener("click", () => {
-      modalOverlay.style.display = "flex";
-    });
-    closeModal.addEventListener("click", () => {
-      modalOverlay.style.display = "none";
-    });
-    window.addEventListener("click", (e) => {
-      if (e.target === modalOverlay) {
-        modalOverlay.style.display = "none";
-      }
-    });
+  const openModalBtn = document.getElementById('contactBtn'); // Тепер Контакти
+  const modalOverlay = document.getElementById('modalOverlay');
+  const closeModalBtn = document.getElementById('closeModal');
+
+  openModalBtn.addEventListener('click', (e) => {
+    e.preventDefault(); // щоб посилання не прокручувало сторінку
+    modalOverlay.style.display = 'flex';
+  });
+
+  closeModalBtn.addEventListener('click', () => {
+    modalOverlay.style.display = 'none';
+  });
+
+  modalOverlay.addEventListener('click', (e) => {
+    if (e.target === modalOverlay) {
+      modalOverlay.style.display = 'none';
+    }
+  });
